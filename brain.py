a94fef7e5c30e3febb0e5d9dfb0839658b20b63a
HaoTse <top30339@gmail.com>
2018-06-07 19:54:35 +0800

    Complete DQN

--- brain.py
+++ brain.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from config import BATCH_SIZE, TARGET_REPLACE_ITER, MEMORY_CAPACITY
 
 class RL(object):
     def __init__(self, action_space, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
@@ -22,6 +27,7 @@ class RL(object):
             )
 
     def choose_action(self, observation):
+        observation = str(observation)
         self.check_state_exist(observation)
         # action selection
         if np.random.rand() < self.epsilon:
@@ -37,7 +43,6 @@ class RL(object):
     def learn(self, *args):
         pass
 
-
 # off-policy
 class QLearningTable(RL):
     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
@@ -51,7 +56,6 @@ class QLearningTable(RL):
         else:
             q_target = r  # next state is terminal
         self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
-
 
 # on-policy
 class SarsaTable(RL):
@@ -67,3 +71,78 @@ class SarsaTable(RL):
         else:
             q_target = r  # next state is terminal
         self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
+
+# DQN
+class Net(nn.Module):
+    def __init__(self, action_n, state_n):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(state_n, 50)
+        self.fc1.weight.data.normal_(0, 0.1)   # initialization
+        self.out = nn.Linear(50, action_n)
+        self.out.weight.data.normal_(0, 0.1)   # initialization
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        actions_value = self.out(x)
+        return actions_value
+
+class DQN(object):
+    def __init__(self, action_n, state_n, env_shape, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.eval_net, self.target_net = Net(action_n=action_n, state_n=state_n), Net(action_n=action_n, state_n=state_n)
+
+        self.action_n = action_n
+        self.state_n = state_n
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.env_shape = env_shape
+
+        self.learn_step_counter = 0  # for target updating
+        self.memory_counter = 0  # for storing memory
+        self.memory = np.zeros((MEMORY_CAPACITY, self.state_n * 2 + 2))
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=self.lr)
+        self.loss_func = nn.MSELoss()
+
+    def choose_action(self, x):
+        x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        # input only one sample
+        if np.random.uniform() < self.epsilon:  # greedy
+            actions_value = self.eval_net.forward(x)
+            action = torch.max(actions_value, 1)[1].data.numpy()
+            action = action[0] if self.env_shape == 0 else action.reshape(self.env_shape)
+        else:  # random
+            action = np.random.randint(0, self.action_n)
+            action = action if self.env_shape == 0 else action.reshape(self.env_shape)
+        return action
+
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def learn(self):
+        # target parameter update
+        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.learn_step_counter += 1
+
+        # sample batch transitions
+        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
+        b_memory = self.memory[sample_index, :]
+        b_s = torch.FloatTensor(b_memory[:, :self.state_n])
+        b_a = torch.LongTensor(b_memory[:, self.state_n:self.state_n+1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, self.state_n+1:self.state_n+2])
+        b_s_ = torch.FloatTensor(b_memory[:, -self.state_n:])
+
+        # q_eval w.r.t the action in experience
+        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
+        q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
+        q_target = b_r + self.gamma * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
---
+++ train.py
@@ -0,0 +1,110 @@
+import gym
+import pandas as pd
+import argparse
+
+from config import MEMORY_CAPACITY, EPISODE_NUM
+from brain import QLearningTable, SarsaTable, DQN
+
+def newReward(obsesrvation, obsesrvation_):
+    return abs(obsesrvation_[0] - (-0.5))
+
+def update(method):
+    records = []
+    for episode in range(EPISODE_NUM):
+        # initial
+        observation = env.reset()
+
+        if method == 'SARSA':
+            action = RL.choose_action(observation)
+
+        iter_cnt, total_reward = 0, 0
+        while True:
+            iter_cnt += 1
+
+            # fresh env
+            env.render()
+
+            if method == 'QL':
+                # RL choose action based on observation
+                action = RL.choose_action(observation)
+                # RL take action and get next observation and reward
+                observation_, reward, done, _ = env.step(action)
+                reward = newReward(observation, observation_)
+                # RL learn from this transition
+                RL.learn(str(observation), action, reward, str(observation_))
+            elif method == 'SARSA':
+                # RL take action and get next observation and reward
+                observation_, reward, done, _ = env.step(action)
+                reward = newReward(observation, observation_)
+                # RL choose action based on observation
+                action_ = RL.choose_action(observation)
+                # learn from trasition (s, a, r, s, a)
+                RL.learn(str(observation), action, reward, str(observation_), action_)
+                # swap action
+                action = action_
+            if method == 'DQN':
+                # RL choose action based on observation
+                action = RL.choose_action(observation)
+                # RL take action and get next observation and reward
+                observation_, reward, done, _ = env.step(action)
+                reward = newReward(observation, observation_)
+                # RL learn from this transition
+                RL.store_transition(observation, action, reward, observation_)
+                if RL.memory_counter > MEMORY_CAPACITY:
+                    RL.learn()
+
+            # accumulate reward
+            total_reward += reward
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                total_reward = round(total_reward, 2)
+                records.append((iter_cnt, total_reward))
+                print("Episode {} finished after {} timesteps, total reward is {}".format(episode + 1, iter_cnt, total_reward))
+                break
+
+    # end of game
+    print('--------------------------------')
+    print('game over')
+    env.close()
+
+    df = pd.DataFrame(records, columns=["iters", "reward"])
+    df.to_csv(method + ".csv", index=False)
+
+if __name__ == "__main__":
+
+    # argument
+    parse = argparse.ArgumentParser()
+    parse.add_argument('-m', '--method',
+                        default='DQN',
+                        help='Choose which rl algorithm used (QL, SARSA, or DQN)')
+    args = parse.parse_args()
+
+    # env setup
+    env = gym.make('MountainCar-v0')
+    env = env.unwrapped
+
+    # algorithm setup
+    method = args.method
+    if method == 'QL':
+        print("Use Q-Learning...")
+        print('--------------------------------')
+        RL = QLearningTable(actions=list(range(env.action_space.n)))
+    elif method == 'SARSA':
+        print("Use SARSA...")
+        print('--------------------------------')
+        RL = SarsaTable(actions=list(range(env.action_space.n)))
+    elif method == 'DQN':
+        print("Use DQN...")
+        print('--------------------------------')
+        env_shape = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape  # to confirm the shape
+        RL = DQN(action_n=env.action_space.n, state_n=env.observation_space.shape[0], env_shape=env_shape)
+    else:
+        print("Error method! Use DQN instead.")
+        print('--------------------------------')
+        env_shape = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape  # to confirm the shape
+        RL = DQN(action_n=env.action_space.n, state_n=env.observation_space.shape[0], env_shape=env_shape)
+
+    update(method)
